93139b528a58a465a8c21de40cf7f497d2f50f63
saramohamed43 <saramohame43@gmail.com>
2025-11-26 16:55:56 +0200

    added sudokuBoard class

---
+++ lab9/src/SudokuBoard.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class SudokuBoard {
+    private final int[][] sudokuGrid = new int [9][9];
+
+    public SudokuBoard(String filePath) throws IOException{
+        load(filePath);
+    }
+
+    private void load(String filePath) throws IOException{
+        BufferedReader br = new BufferedReader(new FileReader(filePath));
+        String line;
+        int row = 0;
+
+        while((line = br.readLine())!=null && row<9){
+            String[] token = line.split(",");
+            for(int column = 0; column<9; column ++){
+                sudokuGrid[row][column] = Integer.parseInt(token[column].trim());
+            }
+            row++;
+        }
+        br.close();
+    }
+
+    public int getCell(int row, int column){
+        return sudokuGrid[row][column];
+    }
+
+    public int[] getRow(int row){
+        return sudokuGrid[row];
+    }
+
+    public int[] getColumn(int column){
+        int[] columnArray = new int[9];
+        for(int row=0; row<9; row++){
+            columnArray[row] = sudokuGrid[row][column];
+        }
+        return columnArray;
+    }
+
+    public int[] getBox(int boxIndex) {
+        int[] box = new int[9];
+        int rowStart = (boxIndex / 3) * 3;
+        int columnStart = (boxIndex % 3) * 3;
+        int i = 0;
+
+        for (int row = rowStart; row < rowStart + 3; row++) {
+            for (int column = columnStart; column < columnStart + 3; column++) {
+                box[i++] = sudokuGrid[row][column];
+            }
+        }
+        return box;
+    }
+}
