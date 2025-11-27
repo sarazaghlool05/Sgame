2673e21a34655d5d5808b219b86e8ab706b31aef
sarazaghlool <es.sara.ahmed2023@alexu.edu.eg>
2025-11-27 23:47:10 +0200

    Update StrategyFactory.java

--- lab9/src/StrategyFactory.java
+++ lab9/src/StrategyFactory.java
@@ -2,7 +2,7 @@ public class StrategyFactory {
     public static VerificationStrategy createStrategy(SudokuBoard board, String mode) {
         switch (mode) {
             case "0": return new ZeroThreadStrategy(board);
-            case "3": return new ThreeThreadStrategy(board);
+            case "3": return new ThreeThreadStrategy();
            case "27": return new TwentySevenThreadStrategy(board);
             default: throw new IllegalArgumentException("Invalid mode");
         }
