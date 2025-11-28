f7704a58d97d29d80fe87f9ee770c04093120607
zeinaelbanna <zeinaelbanna5@gmail.com>
2025-11-29 00:05:03 +0200

    the test case is working

--- lab9/src/TestBoard.java
+++ lab9/src/TestBoard.java
@@ -1,15 +1,21 @@
 public class TestBoard {
     public static void main(String[] args) {
-        String file = "Board.csv";
+        String file = "board.csv";
         String[] modes = new String[]{"0", "3", "27"};
+
         for (String mode : modes) {
             System.out.println("Mode " + mode + ":");
             try {
                 SudokuBoard board = new SudokuBoard(file);
-                VerificationStrategy strategy = StrategyFactory.createStrategy(board,mode);
-                strategy.verify(board);
+                VerificationStrategy strategy = StrategyFactory.createStrategy(board, mode);
+                boolean valid = strategy.verify(board);
                 ValidationResult result = new ValidationResult();
+                for (Duplicate d : strategy.returnDuplicates()) {
+                    result.addDuplicate(d);
+                }
+
                 ResultPrinting.printResult(result);
+                System.out.println("----------------------------------");
             }
             catch (Exception e) {
                 System.out.println("Error: " + e.getMessage());
