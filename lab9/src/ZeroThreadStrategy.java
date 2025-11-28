5e679da0c8c2254ff678bf98ee68a40f543ed015
saramohamed43 <saramohame43@gmail.com>
2025-11-28 23:08:53 +0200

    fixed zeroModeStrategy

--- lab9/src/ZeroThreadStrategy.java
+++ lab9/src/ZeroThreadStrategy.java
@@ -1,22 +1,40 @@
-public class ZeroThreadStrategy implements Validator{
+import java.util.ArrayList;
+import java.util.List;
+
+public class ZeroThreadStrategy implements VerificationStrategy {
     private SudokuBoard board;
+    private List<Duplicate> allDuplicates;
 
     public ZeroThreadStrategy(SudokuBoard board) {
         this.board = board;
+        this.allDuplicates = new ArrayList<>();
     }
 
     @Override
-    public ValidationResult validate() {
-        ValidationResult result = new ValidationResult();
+    public boolean verify(SudokuBoard board) {
+        allDuplicates.clear();
 
-        RowValidator rowValidator = new RowValidator(board, result);
-        ColumnValidator columnValidator = new ColumnValidator(board, result);
-        BoxValidator boxValidator = new BoxValidator(board, result);
+        // Create validators - they create their own internal results
+        RowValidator rowValidator = new RowValidator(board);
+        ColumnValidator columnValidator = new ColumnValidator(board);
+        BoxValidator boxValidator = new BoxValidator(board);
 
-        rowValidator.validateRows();
-        columnValidator.validateColumns();
-        boxValidator.validateBoxes();
+        // Perform validations
+        rowValidator.performValidation();
+        columnValidator.performValidation();
+        boxValidator.performValidation();
 
-        return result;
+        // Collect all duplicates from each validator
+        allDuplicates.addAll(rowValidator.validate().getDuplicates());
+        allDuplicates.addAll(columnValidator.validate().getDuplicates());
+        allDuplicates.addAll(boxValidator.validate().getDuplicates());
+
+        // Return true if no duplicates found (valid board)
+        return allDuplicates.isEmpty();
+    }
+
+    @Override
+    public List<Duplicate> returnDuplicates() {
+        return new ArrayList<>(allDuplicates);
     }
 }
